79cccd9534aba8c7f3069084acced7a5a910aebb
prettykaur <pkaur.dev@gmail.com>
2023-06-27 22:57:21 +0800

    Install axios

--- controllers/restaurantsController.js
+++ controllers/restaurantsController.js
@@ -1,10 +1,27 @@
 /* eslint-disable no-unused-vars */
 const BaseController = require("./baseController");
+const axios = require("axios");
 
 class RestaurantsController extends BaseController {
   constructor(model) {
     super(model);
   }
+
+  // Get restaurants from search
+
+  // Get restaurant details from API
+
+  // Add restaurant to database
+
+  // Update restaurant info
+
+  // Delete restaurant
+
+  // Upvote restaurant
+
+  // Count upvotes for restaurant
+
+  // Remove upvote for restaurant
 }
 
 module.exports = RestaurantsController;
