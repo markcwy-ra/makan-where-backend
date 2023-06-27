3eb76f5b41493598a1576b494c19f5bb7301b8c1
prettykaur <pkaur.dev@gmail.com>
2023-06-27 21:22:18 +0800

    Create users methods and routes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 const BaseController = require("./baseController");
+const { Op } = require("sequelize");
 
 class UsersController extends BaseController {
   constructor(model) {
@@ -7,12 +8,98 @@ class UsersController extends BaseController {
   }
 
   // Get user profile
+  getUserProfile = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+      // Get user's profile from database
+      const user = await this.model.findByPk(userId);
+
+      if (!user) {
+        return res.status(404).json({ error: true, msg: "User not found" });
+      }
+
+      return res.json(user);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  };
 
   // Update user profile
+  updateUserProfile = async (req, res) => {
+    const { userId } = req.params;
+    // const { username, email, address, photoUrl } = req.body;
+
+    try {
+      // Get user from db
+      const user = await this.model.findByPk(userId);
+
+      if (!user) {
+        return res.status(404).json({ error: true, msg: "User not found" });
+      }
+
+      await user.update(req.body);
+
+      const updatedUser = await this.model.findByPk(userId);
+
+      return res.json(updatedUser);
+    } catch (err) {
+      console.log("Error updating user profile:", err);
+      return res.status(400).json({ error: true, msg: err.message });
+    }
+  };
 
   // Delete user
+  deleteUser = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+      const user = await this.model.findByPk(userId);
+
+      if (!user) {
+        return res.status(404).json({ error: true, msg: "User not found" });
+      }
+
+      const deletedUser = await this.model.destroy({
+        where: {
+          id: userId,
+        },
+      });
+
+      return res.json({
+        success: true,
+        msg: "User deleted successfully",
+        user: deletedUser.userId,
+      });
+    } catch (err) {
+      console.log("Error deleting user:", err);
+      return res.status(400).json({ error: true, msg: err });
+    }
+  };
 
   // Get search results for users by username
+  searchUsersByUsername = async (req, res) => {
+    const { username } = req.params;
+
+    try {
+      // Find all users whose username matches search term
+      const users = await this.model.findAll({
+        where: {
+          username: {
+            [Op.like]: `%${username}%`,
+          },
+        },
+      });
+
+      if (users.length === 0) {
+        return res.status(404).json({ error: true, msg: "No users found" });
+      }
+
+      return res.json(users);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  };
 }
 
 module.exports = UsersController;
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -2,11 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 class UsersRouter {
-  constructor(controller) {
+  constructor(controller, verifyToken) {
     this.controller = controller;
+    this.verifyToken = verifyToken;
   }
 
   routes() {
+    router.get("/:userId", this.controller.getUserProfile);
+    router.put(
+      "/:userId/update",
+      this.verifyToken,
+      this.controller.updateUserProfile
+    );
+    router.delete("/:userId", this.verifyToken, this.controller.deleteUser);
+
+    router.get("/search/:username", this.controller.searchUsersByUsername);
+
     return router;
   }
 }
