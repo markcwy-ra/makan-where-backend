ff9f7c697af03a5a1cfe50eaaaacd77533486558
markcwy-ra <rocket@markcwy.com>
2023-06-28 14:55:57 +0800

    Added user info in response on signup/in

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 const corsOptions = {
