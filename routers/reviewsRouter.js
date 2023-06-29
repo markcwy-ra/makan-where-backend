e1b5eeae45ea391c4c184298b6fd7fc5d00dc5c3
prettykaur <pkaur.dev@gmail.com>
2023-06-29 15:16:50 +0800

    Add routes to review restaurants

--- routers/reviewsRouter.js
+++ routers/reviewsRouter.js
@@ -8,6 +8,35 @@ class ReviewsRouter {
   }
 
   routes() {
+    router.get("/:restaurantId/count", this.controller.countAllReviews);
+    router.get("/:restaurantId", this.controller.getAllReviews);
+
+    router.get("/user/:userId/count", this.controller.countAllUserReviews);
+    router.get("/user/:userId", this.controller.getAllUserReviews);
+    router.get(
+      "/restaurant/:restaurantId/user/:userId",
+      this.controller.getOneUserReview
+    );
+
+    router.post("/:restaurantId/add", this.controller.addReview);
+    router.put("/:reviewId/update", this.controller.updateReview);
+    router.delete("/:reviewId", this.controller.deleteReview);
+
+    router.delete(
+      "/:reviewId/upvote/remove",
+      this.controller.removeReviewUpvote
+    );
+    router.post("/:reviewId/upvote", this.controller.upvoteReview);
+
+    router.get("/:reviewId/upvotes/count", this.controller.countReviewUpvotes);
+    router.get("/:reviewId/upvotes", this.controller.getReviewUpvotes);
+
+    router.get(
+      "/user/:userId/upvotes/count",
+      this.controller.countUserUpvotedReviews
+    );
+    router.get("/user/:userId/upvotes", this.controller.getUserUpvotedReviews);
+
     return router;
   }
 }
